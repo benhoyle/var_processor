0645a1839bd8310ef793030660b8021c3055eb6b
Ben Hoyle <benjhoyle@gmail.com>
2020-03-30 10:40:38 +0100

    Covariance test

--- src/tests/test_vpu.py
+++ src/tests/test_vpu.py
@@ -32,6 +32,24 @@ def test_covariance_unit():
     assert cov_unit.covariance.any()
 
 
+def test_covariance_computation():
+    """Statistical test that cov unit is determining the covariance."""
+    # Generate random positive definite matrix
+    cov = np.random.randn(3, 3)
+    cov = np.dot(cov, cov.T)
+    # Generate desired mean
+    mean = np.random.randn(3, 1)
+    # Use Cholesky decomposition to get L
+    L = np.linalg.cholesky(cov)
+    cov_unit = CovarianceUnit(3)
+    for _ in range(0, 10000):
+        sample = np.dot(L, np.random.randn(3, 1)) + mean
+        cov_unit.update(sample)
+    # Check within 10%
+    assert np.allclose(mean, cov_unit.mean, rtol=0.10)
+    assert np.allclose(cov, cov_unit.covariance, rtol=0.10)
+
+
 def test_power_iterator():
     """Test power iterator."""
     # Test initialise
