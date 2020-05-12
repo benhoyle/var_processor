1d838aef6e9688e295904d7f2b734c32839565b4
Ben Hoyle <benjhoyle@gmail.com>
2020-05-12 15:19:24 +0100

    Video pre-processing methods

--- src/sources/video.py
+++ src/sources/video.py
@@ -3,12 +3,29 @@
 Some more advanced pre-processing for video sources.
 """
 import threading
-import numpy as np
 
 # Video
 import cv2
 
 from src.sources.abstract import SensorSource
+
+
+def flatten_frame(frame, vec_len):
+    """Flatten a frame of video to a 1D array."""
+    rows = frame.shape[0] // vec_len
+    cols = frame.shape[1] // vec_len
+    flattened = frame.reshape(
+        rows, vec_len, cols, vec_len).swapaxes(2, 1).flatten()
+    return flattened
+
+
+def reconstruct_frame(array, vec_len, rows, cols):
+    """Reconstruct a 1D array of clustered blocks into a 2D image.
+
+    rows, cols are original shape."""
+    return array.reshape(
+        rows//vec_len, cols//vec_len, vec_len, vec_len
+    ).swapaxes(2, 1).reshape(rows, cols)
 
 
 def separate_components(frame, square=True):
@@ -17,9 +34,10 @@ def separate_components(frame, square=True):
     square - boolean - if true subsample the colour images so they are square
     """
     if square:
-        return frame[:, :, 0], frame[::2, 1::2, 1], frame[::2, 0::2, 1]
+        components = frame[:, :, 0], frame[::2, 1::2, 1], frame[::2, 0::2, 1]
     else:
-        return frame[:, :, 0], frame[:, 1::2, 1], frame[:, 0::2, 1]
+        components = frame[:, :, 0], frame[:, 1::2, 1], frame[:, 0::2, 1]
+    return components
 
 
 class VideoSource(SensorSource):
@@ -70,3 +88,14 @@ class VideoSource(SensorSource):
     def __exit__(self, exec_type, exc_value, traceback):
         """Extra code to close camera."""
         self.cap.release()
+
+
+class YSource(VideoSource):
+    """Version of video source that just returns the lightness channel."""
+
+    def read(self):
+        """Read video."""
+        with self.read_lock:
+            frame = self.frame.copy()
+            grabbed = self.grabbed
+        return grabbed, frame[..., 0]
--- src/tests/test_capture_video.py
+++ src/tests/test_capture_video.py
@@ -2,8 +2,9 @@
 import time
 import numpy as np
 
-from src.sources.video import separate_components, VideoSource
-
+from src.sources.video import (
+    flatten_frame, reconstruct_frame, separate_components, VideoSource
+)
 
 def test_separate_components():
     """Test separate components method."""
@@ -20,6 +21,28 @@ def test_separate_components():
     assert (one == 1).all()
     assert (two == 2).all()
     assert (three == 3).all()
+
+
+def test_flatten_reconstruct():
+    # Build test array
+    test = np.arange(64).reshape(8,8)
+    # Test flattening in blocks of 4x4
+    flat4 = flatten_frame(test, 4)
+    assert flat4.shape[0] == 64
+    assert len(flat4.shape) == 1
+    assert np.array_equal(flat4[0:4], np.arange(4))
+    assert np.array_equal(flat4[4:8], np.asarray([8,  9,  10,  11]))
+    # Test flattening in blocks of 2x2
+    flat2 = flatten_frame(test, 2)
+    assert flat2.shape[0] == 64
+    assert len(flat2.shape) == 1
+    assert np.array_equal(flat2[0:4], np.asarray([0,  1,  8,  9]))
+    assert np.array_equal(flat2[4:8], np.asarray([2,  3, 10, 11]))
+    # Test reconstruction
+    reconstructed4 = reconstruct_frame(flat4, 4, 8, 8)
+    assert np.array_equal(reconstructed4, test)
+    reconstructed2 = reconstruct_frame(flat2, 2, 8, 8)
+    assert np.array_equal(reconstructed2, test)
 
 
 def test_video_source():
